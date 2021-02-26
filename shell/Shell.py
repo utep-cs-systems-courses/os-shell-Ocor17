33683d670c8406d4ad0dc6afa697acdbad724810
ShAadow0 <oscarnerf@gmail.com>
2021-02-26 13:17:57 -0700

    file sync

--- shell/Shell.py
+++ shell/Shell.py
@@ -1,17 +1,35 @@
 #! /usr/bin/env python3
 
 import os, sys, time, re
+
+def execve(args):    
+        #print("-------------",args)
+        for dir in re.split(":", os.environ['PATH']): # try each directory in the path
+            program = "%s/%s" % (dir, args[0])
+            #os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
+            try:
+                os.execve(program, args, os.environ) # try to exec program
+            except FileNotFoundError:             # ...expected
+                pass                              # ...fail quietly
+
+        os.write(2, ("Could not exec %s\n" % args[0]).encode())
+        sys.exit(1)                 # terminate with error
+
 p_read, p_write = os.pipe()
+
+PS1 ="$ "
+if ((os.environ.get('PS1'))!= None):
+    PS1 = os.environ.get('PS1') 
 
 for f in (p_read, p_write):
     os.set_inheritable(f, True)
 
 while True:
 
-    os.write(1, ((os.getcwd()+("$ ")).encode()))
-
-    command = os.read(0,1000)
-    args = re.split("[ \n]", command.decode())
+    if in_pipe == False:
+        os.write(1, ((os.getcwd()+(PS1)).encode()))
+        command = os.read(0,1000)
+        args = re.split("[ \n]", command.decode())
 
     if args[0] == "exit":
         os.write(1, "exiting...\n".encode())
@@ -36,37 +54,26 @@ while True:
     elif rc == 0:                   # child
         if '>' in args:
             os.close(1)
-            os.open(args[args.index('>')+1], os.O_CREAT | os.O_WRONLY);
-            os.set_inheritable(1,True)
-            args.remove(args[args.index('>')+1])
+            os.open(args[args.index('>')+1], os.O_CREAT | os.O_WRONLY); #create file to write to
+            os.set_inheritable(1,True) # > needs display fd
+            args.remove(args[args.index('>')+1]) #prep for more
             args.remove('>')
 
         if '<' in args:
             os.close(0)
             os.open(args[args.index('<')+1], os.O_RDONLY)
-            os.set_inheritable(0,True)
+            os.set_inheritable(0,True) # < needs keyboard fd
             args.remove(args[args.index('<')+1])
             args.remove('<')
 
         if '|' in args:
             os.close(1)
             os.set_inheritable(os.dup(p_write),True)
-            args = args[:args.index('|')]
+            args = args[:args.index('|')] #seperate pipes to get ready to handle them sequentially
             for fd in (p_read,p_write):
                 os.close(fd)
 
-            
-        #print("-------------",args)
-        for dir in re.split(":", os.environ['PATH']): # try each directory in the path
-            program = "%s/%s" % (dir, args[0])
-            #os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
-            try:
-                os.execve(program, args, os.environ) # try to exec program
-            except FileNotFoundError:             # ...expected
-                pass                              # ...fail quietly
-
-        os.write(2, ("Could not exec %s\n" % args[0]).encode())
-        sys.exit(1)                 # terminate with error
+        execve(args)
 
     else:                           # parent (forked ok)
         os.wait()
